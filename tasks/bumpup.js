87de3059b9e555c046d80be251b78e97a39f2d3c
Marcin Antczak <marcin.antczak@neutrico.pl>
2013-12-01 18:48:07 +0100

    Fixed regexp

--- tasks/bumpup.js
+++ tasks/bumpup.js
@@ -122,11 +122,10 @@ module.exports = function(grunt) {
       // This will match quoted and unquoted 'version' or Version and PHP syntax arrays =>
 
     // var VERSION_REGEXP = /([\'|\"]?[Vv]ersion[\'|\"]?[ ]*[:|=|=][>]?[ ]*[\'|\"]?)([\d||A-a|.|-]*)([\'|\"]?)/i;
-
-    var VERSION_REGEXP = /([\'|\"]?[@]?[Vv]ersion[\'|\"]?[ ]*[:|=|=]?[>]?[ ]*[\'|\"]?)([\d||A-a|.|-]*)([\'|\"]?)/i;
+    // var VERSION_REGEXP = /([\'|\"]?[@]?[Vv]ersion[\'|\"]?[ ]*[:|=|=]?[>]?[ ]*[\'|\"]?)([\d||A-a|.|-]*)([\'|\"]?)/i;
     // var VERSION_REGEXP = /(\bversion[\'\"]?\s*[:=]\s*[\'\"])([\da-z\.-]+)([\'\"])/i;
 
-
+    var VERSION_REGEXP = /([\'|\"]?[*]?[ ]?[@]?[Vv]ersion[\'|\"]?[ ]*[:|=|=]?[>]?[ ]*[\'|\"]?)([\d||A-a|.|-]*)([\'|\"]?)/i;
 
     // GET VERSION FROM GIT
     runIf(opts.bumpVersion && versionType === 'git', function(){
